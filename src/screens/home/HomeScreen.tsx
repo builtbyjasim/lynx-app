6dbc804c06489387be3abc075ab64cf73e31baf2
builtbyjasim <zasimkhan3@gmail.com>
2026-02-26 16:28:19 +0530

    isAndroid

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import { isAndroid } from "../../utils/platform/isAndroid";
 import { SafeArea } from "../components/SafeArea";
 import "./HomeScreen.css";
 
@@ -5,8 +6,10 @@ export default function HomeScreen() {
   return (
     <SafeArea>
       <view className="HomeScreen">
-        <text>Home</text>
-        <view style={{ flex: 1, background: "red",  }} />
+        <text>Home </text>
+        <text>{JSON.stringify(SystemInfo.platform)}</text>
+        <text>{JSON.stringify(SystemInfo)}</text>
+        <view style={{ flex: 1, background: "red" }} />
       </view>
     </SafeArea>
   );
---
+++ src/utils/platform/isAndroid.ts
@@ -0,0 +1,3 @@
+export const isAndroid = () => {
+  return SystemInfo.platform == "Android";
+};
